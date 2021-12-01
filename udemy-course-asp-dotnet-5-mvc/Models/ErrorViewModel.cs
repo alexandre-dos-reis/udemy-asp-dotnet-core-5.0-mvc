5d25f2fd8fac27775487a8668e54d7ba1ef3414f
Alex <ajm.dosreis.daponte@gmail.com>
2021-12-01 23:40:34 +0100

    Initial commit

---
+++ udemy-course-asp-dotnet-5-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace udemy_course_asp_dotnet_5_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
